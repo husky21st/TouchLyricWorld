a02197acec4206299452a2b2f9b3243f71cdf4bf
Husky21st <husky21.mn@gmail.com>
2021-09-17 13:53:38 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  //for GitHub Pages
+  assetPrefix: process.env.CI ? '/TouchLyricWorld': undefined,
+  distDir: 'build',
   reactStrictMode: true,
+  poweredByHeader: false,
+  optimizeFonts: true,
 }
